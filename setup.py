478bb2e82ee2ebce7b44d560fa9cf30b4e317c32
Mafinar Khan <mafinar.khan@bdpanacea.com>
2015-03-22 20:32:08 +0600

    Fixed some issues with setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 setup(
     name='hydjack',
     version='0.1',
@@ -13,7 +8,7 @@ setup(
     include_package_data=True,
     license='MIT License',
     description='Django just got Hy',
-    long_description=README,
+    #long_description=README,
     url='https://github.com/code-shoily/hydjack',
     author='Code Shoily',
     author_email='code.shoily@gmail.com',
